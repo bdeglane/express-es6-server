cd72c0e6995acd15e62f19ba7a6d103e567009db
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-12-12 15:41:13 +0100

    major change add Map controllers add getter and setter for resource and resourceName add method init controller for Map add method get controller

--- core/controller/Controller.js
+++ core/controller/Controller.js
@@ -1,12 +1,76 @@
 'use strict';
 
+import BaseController from './BaseController';
+
+// store resource controllers to avoid duplicate memory entry
+let controllers = new Map();
+
 export default class Controller {
   /**
    * Resource controller constructor
    * @param Resource {object}
    */
   constructor(Resource) {
-    this.resource = new Resource()
+    this.resourceName = '';
+    this.resource = Resource;
+    this.initControllerClass();
+  }
+
+  /**
+   *
+   * @param R {function}
+   */
+  set resource(R) {
+    if (R.prototype instanceof BaseController) {
+      this._resource = R;
+    } else {
+      throw new TypeError('resource must be inherit from BaseController');
+    }
+  }
+
+  /**
+   *
+   * @returns {Function}
+   */
+  get resource() {
+    return this._resource;
+  }
+
+  /**
+   *
+   * @param name {string}
+   */
+  set resourceName(name) {
+    this._resourceName = name;
+  }
+
+  /**
+   * @returns {string}
+   */
+  get resourceName() {
+    return this._resourceName;
+  }
+
+  initControllerClass() {
+    // instanciate the class controller
+    let resource = new this.resource();
+    // if key of current resource doesn't exist
+    if (!controllers.has(resource.resource)) {
+      // create one in map controllers
+      controllers.set(resource.resource, resource);
+    }
+    // then save name of the resource controller
+    this.resourceName = resource.resource;
+  }
+
+  /**
+   *
+   * @returns {object}
+   */
+  getController() {
+    if (controllers.has(this.resourceName)) {
+      return controllers.get(this.resourceName);
+    }
   }
 
   /**
@@ -39,7 +103,7 @@ export default class Controller {
    */
   method({req, res, next, method}) {
     if (this.hasMethod(method)) {
-      this.resource[method](req, res);
+      this.getController()[method](req, res);
     } else {
       // todo write error class as a service
       res.send('sad');
@@ -53,6 +117,6 @@ export default class Controller {
    * @returns {boolean}
    */
   hasMethod(method) {
-    return typeof this.resource[method] === 'function';
+    return typeof this.getController()[method] === 'function';
   }
 }
