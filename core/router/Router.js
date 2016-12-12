2fee3b8c0e203ff95580fc158611b6dce613c069
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-12-12 13:52:57 +0100

    add setter and getter add type verification

--- core/router/Router.js
+++ core/router/Router.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import Route from './Route';
+import BaseController from '../controller/BaseController';
 
 export default class Router {
   /**
@@ -10,18 +11,77 @@ export default class Router {
    */
   constructor({controller, routing}) {
     this.resource = controller;
-    this.routes = [];
-    this.build(routing, controller);
+    this.routing = routing;
+    this._routes = [];
+    // build a Route class for each
+    // literal object in routing
+    this.build();
+  }
+
+  /**
+   * A class extending BaseControlleur
+   *
+   * @param r {function}
+   */
+  set resource(r) {
+    if (r.prototype instanceof BaseController) {
+      this._resource = r;
+    } else {
+      throw new TypeError('resource must be inherit from BaseController');
+    }
   }
 
   /**
    *
-   * @param routes {array}
-   * @param controller {object}
+   * @returns {function}
    */
-  build(routes, controller) {
-    for (let route in routes) {
-      this.routes.push(new Route(routes[route], controller));
+  get resource() {
+    return this._resource;
+  }
+
+  /**
+   *
+   * @param r {object}
+   */
+  set routing(r) {
+    if (Array.isArray(r)) {
+      this._routing = r;
+    } else {
+      throw new TypeError('resource must be an array');
+    }
+  }
+
+  /**
+   *
+   * @returns {object}
+   */
+  get routing() {
+    return this._routing;
+  }
+
+  /**
+   *
+   * @param r {Array}
+   */
+  set routes(r) {
+    throw new Error('this.routes is a private attribute');
+  }
+
+  /**
+   *
+   * @returns {Array}
+   */
+  get routes() {
+    return this._routes;
+  }
+
+  /**
+   * push in this.routes array an instance of
+   * Route for each route in this.routing
+   **/
+  build() {
+    for (let route in this.routing) {
+      this.routes.push(new Route(this.routing[route], this.resource));
     }
   }
 
