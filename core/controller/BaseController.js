0150480cb51632dd2b0b021ad1945c3654be18a9
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-12-12 15:39:33 +0100

    add getter and setter for resource

--- core/controller/BaseController.js
+++ core/controller/BaseController.js
@@ -4,8 +4,28 @@ import ServiceContainer from '../service/ServiceContainer';
 
 export default class BaseController {
   constructor(resource) {
-    this.resource = resource;
+    this._resource = resource;
     this.service = ServiceContainer.getInstance();
+  }
+
+  /**
+   *
+   * @param r {string}
+   */
+  set resource(r) {
+    if (typeof r === 'string') {
+      this._resource = r;
+    } else {
+      throw new TypeError('resource must be a string');
+    }
+  }
+
+  /**
+   *
+   * @returns {string}
+   */
+  get resource() {
+    return this._resource;
   }
 
   // leave blank
