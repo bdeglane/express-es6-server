9838cfd6d0cc2b08608c4b1a9f5d4713d6781e05
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-12-19 16:44:18 +0100

    add helper

---
+++ app/Controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import BaseController from '../core/controller/BaseController';
+
+export default class Controller extends BaseController {
+	constructor(resource) {
+		super(resource);
+	}
+
+	// leave blank
+}
---
+++ app/Dao.js
@@ -0,0 +1,9 @@
+import BaseDao from '../core/dao/BaseDao';
+
+export default class Dao extends BaseDao {
+  constructor() {
+    super();
+  }
+
+  // leave blank
+}
