d6556b80819e56bd7926f691a86a4307d4a86e90
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-12-21 10:46:00 +0100

    response data is now a literal object

--- core/view/View.js
+++ core/view/View.js
@@ -1,40 +1,40 @@
 export default class View {
-	constructor() {
-		this.response = {
-			data: [],
-			status: -1,
-			error: {status: false}
-		};
-	}
+  constructor() {
+    this.response = {
+      data: {},
+      status: -1,
+      error: {status: false}
+    };
+  }
 
-	/**
-	 *
-	 * @param data
-	 * @returns {View}
-	 */
-	write(data) {
-		this.response.data = data;
-		return this;
-	}
+  /**
+   *
+   * @param data
+   * @returns {View}
+   */
+  write(data) {
+    this.response.data = data;
+    return this;
+  }
 
-	/**
-	 *
-	 * @param error
-	 * @returns {View}
-	 */
-	writeError(error) {
-		this.response.error.message = error;
-		this.response.error.status = true;
-		return this;
-	}
+  /**
+   *
+   * @param error
+   * @returns {View}
+   */
+  writeError(error) {
+    this.response.error.message = error;
+    this.response.error.status = true;
+    return this;
+  }
 
-	/**
-	 *
-	 * @param status {number}
-	 * @returns {View}
-	 */
-	setStatus(status) {
-		this.response.status = status;
-		return this;
-	}
+  /**
+   *
+   * @param status {number}
+   * @returns {View}
+   */
+  setStatus(status) {
+    this.response.status = status;
+    return this;
+  }
 }
