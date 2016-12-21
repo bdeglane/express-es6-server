68072a6a130af06d47ceb286782d37d0b40acaae
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-12-21 10:44:30 +0100

    add custom error handler for 404 and 500

--- core/kernel.js
+++ core/kernel.js
@@ -45,3 +45,21 @@ export const services = {
   dao: Dao.getInstance(),
   view: View
 };
+
+import {NOT_FOUND, INTERNAL_ERROR} from '../app/Controller';
+
+// deal with 404 and 500
+export const customErrorHandler = {
+  notFound: (req, res) => {
+    let view = new View();
+    view.writeError('not found').setStatus(NOT_FOUND);
+    res.status(view.response.status).json(view.response);
+  },
+  internalServerError: (err, req, res, next) => {
+    // todo log error
+    console.error(err.stack);
+    let view = new View();
+    view.writeError('Something broke!').setStatus(INTERNAL_ERROR);
+    res.status(view.response.status).json(view.response);
+  }
+};
