25a8bcd7f8c5c92d05ba62bf5ba6c8e78750dd9d
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-12-21 11:16:16 +0100

    remove todo

--- core/main.js
+++ core/main.js
@@ -1,5 +1,5 @@
 import Server from './server/Server';
-// todo give all this to server as params
+
 import {config} from '../config/config';
 import {publicResources, privateResources, middlewares, services, customErrorHandler} from './kernel';
 
