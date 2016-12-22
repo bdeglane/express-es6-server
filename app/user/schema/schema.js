03dd8ef2aa947f509f7c42bffdf646cfb7931c86
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-12-22 14:57:06 +0100

    fix error on last promise

--- app/user/schema/schema.js
+++ app/user/schema/schema.js
@@ -4,6 +4,7 @@ import {userSchema} from '../model/UserModel';
 import {roleSchema} from '../model/RoleModel';
 import {sessionSchema} from '../model/SessionModel';
 import {permissionSchema} from '../model/PermissionModel';
+import {rolePermissionSchema} from '../model/rolePermission';
 
 export const createSchema = (knex, callback) => {
   callback = callback || function () {
@@ -11,17 +12,20 @@ export const createSchema = (knex, callback) => {
   knex.schema
   // drop table user, role, session, permission if exist
     .dropTableIfExists('user')
+    .dropTableIfExists('role_permission')
+    .dropTableIfExists('permission')
     .dropTableIfExists('role')
     .dropTableIfExists('session')
-    .dropTableIfExists('permission')
     // then recreate it
     .then(() => roleSchema(knex))
     .then(() => userSchema(knex))
     .then(() => sessionSchema(knex))
     .then(() => permissionSchema(knex))
+    .then(() => rolePermissionSchema(knex))
     .then(() => {
       logger.info('Database schema have been updated');
       callback();
+      return null;
     })
     .catch((err) => {
       logger.error('Create schema error: %s', err.toString());
