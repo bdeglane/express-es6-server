f2ff39fe8b733ea03388e6e8e8d3d2de2647f122
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-12-23 11:46:22 +0100

    add production key for test

--- config/config.js
+++ config/config.js
@@ -18,5 +18,23 @@ export const config = {
       user: "test",
       password: "test"
     }
+  }, production: {
+    app: {
+      // port the node instance listening
+      port: 3010,
+      // concerning the json web token
+      // for auth users
+      token: {
+        secret: "secret",
+        expire: 84000
+      }
+    },
+    database: {
+      host: "192.168.99.100",
+      port: "5432",
+      database: "test_db",
+      user: "test",
+      password: "test"
+    }
   }
 };
