ab78b5b5e9c597ac1bc846c2c3dfd5fd132cf74d
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-12-30 11:11:11 +0100

    remove database sql file

--- database/sql/sequence.sql
+++
@@ -1,4 +0,0 @@
-CREATE SEQUENCE role_id_seq start 1 INCREMENT BY 1;
-CREATE SEQUENCE user_id_seq start 1 INCREMENT BY 1;
-CREATE SEQUENCE session_id_seq start 1 INCREMENT BY 1;
-CREATE SEQUENCE permission_id_seq start 1 INCREMENT BY 1;
